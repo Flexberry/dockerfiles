1324e168361643d49b5a3f386b5de50660c9233b
Alexey Kostarev <kaf@ics.perm.ru>
2018-11-20 15:16:13 +0500

    Add README_ru.md, runContainer.sh. Tune .env

---
+++ pentaho/README_ru.md
@@ -0,0 +1,7 @@
+# Flexberry/pentaho
+
+Image based on [zhicwu/biserver-ce:8.0 Dockerfile](https://hub.docker.com/r/zhicwu/biserver-ce/)
+
+The following features are added to this image:
+- additional russian localization of Saiku intarface;
+- ClickHouse 0.1 driver;
---
+++ pentaho/runContainer.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+. .env
+
+docker run -d \
+  --name $BI_CONTAINER_NAME \
+  -p "${SERVER_PROM_PORT}:1234" \
+  -p "${SERVER_HTTP_PORT}:8080" \
+  -p "${SERVER_AJP_PORT}:8009" \
+  -v  pentaho_pentaho_hidden:/biserver-ce/.pentaho/ \
+  -v  pentaho_hsqldb:/biserver-ce/data/hsqldb/ \
+  -v  pentaho_logs:/biserver-ce/tomcat/logs \
+  -v  pentaho_repository:/biserver-ce/pentaho-solutions/system/jackrabbit/repository \
+  -v  pentaho_tmp:/biserver-ce/tmp \
+  -e APPLY_PATCHES='Y' \
+  -e BI_JAVA_OPTS="${BI_JAVA_OPTS}" \
+  -e HOST_USER_ID=${HOST_USER_ID} \
+  -e STORAGE_TYPE=${STORAGE_TYPE} \
+  -e PDI_HADOOP_CONFIG=${PDI_HADOOP_CONFIG} \
+  -e PDI_MAX_LOG_LINES=${PDI_MAX_LOG_LINES} \
+  -e PDI_MAX_LOG_TIMEOUT=${PDI_MAX_LOG_TIMEOUT} \
+  -e PDI_MAX_OBJ_TIMEOUT=${PDI_MAX_OBJ_TIMEOUT} \
+  -e SERVER_NAME=${SERVER_NAME} \
+  -e SERVER_HOST=${SERVER_HOST} \
+  -e SERVER_PORT=${SERVER_EXT_PORT} \
+  flexberry/pentaho$BI_IMAGE_TAG
