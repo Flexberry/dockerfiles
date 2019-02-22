39dff139a8cf8c1c31f80fcf99fad23109bb14e7
Tsar Nikolay <nsmirnov@ics.perm.ru>
2019-02-22 11:12:02 +0500

    Pentaho Server 8.1: available to configurate db names.

--- pentaho-server/8.1/8.1-postgres/entrypoint.sh
+++ pentaho-server/8.1/8.1-postgres/entrypoint.sh
@@ -3,13 +3,16 @@ set -e
 
 DB_ADMIN_USER=${DB_ADMIN_USER:-postgres}
 DB_ADMIN_PASS=${DB_ADMIN_PASS:-p@ssw0rd}
+JCR_DB_NAME=${JCR_DB_NAME:-jackrabbit}
 JCR_PASS=${JCR_PASS:-password}
+HIBERNATE_DB_NAME=${HIBERNATE_DB_NAME:-hibernate}
 HIBERNATE_PASS=${HIBERNATE_PASS:-password}
+QUARTZ_DB_NAME=${QUARTZ_DB_NAME:-quartz}
 QUARTZ_PASS=${QUARTZ_PASS:-password}
 DB_HOST=${DB_HOST:-postgres}
 DB_PORT=${DB_PORT:-5432}
 
-function wait_database() {  
+function wait_database() {
   echo -n "-----> waiting for database server on $DB_HOST:$DB_PORT ..."
   while ! nc -w 1 $DB_HOST $DB_PORT 2>/dev/null
   do
@@ -24,65 +27,114 @@ function setup_database() {
   echo "-----> setup databases"
   echo "DB_ADMIN_USER: ${DB_ADMIN_USER}"
   echo "DB_ADMIN_PASS: ${DB_ADMIN_PASS}"
+  echo "JCR_DB_NAME: ${JCR_DB_NAME}"
   echo "JCR_PASS: ${JCR_PASS}"
+  echo "HIBERNATE_DB_NAME: ${HIBERNATE_DB_NAME}"
   echo "HIBERNATE_PASS: ${HIBERNATE_PASS}"
+  echo "QUARTZ_DB_NAME: ${QUARTZ_DB_NAME}"
   echo "QUARTZ_PASS: ${QUARTZ_PASS}"
   echo "DB_HOST: ${DB_HOST}"
   echo "DB_PORT: ${DB_PORT}"
 
   wait_database
 
-  sed -i "s/\*\*port\*\*/${DB_PORT}/g" $PENTAHO_HOME/configs/repository.xml 
-  sed -i "s/\*\*host\*\*/${DB_HOST}/g" $PENTAHO_HOME/configs/repository.xml   
-  sed -i "s/\*\*jcr_password\*\*/${JCR_PASS}/g" $PENTAHO_HOME/configs/repository.xml 
-  cp -fv $PENTAHO_HOME/configs/repository.xml \
+  repository=$PENTAHO_HOME/configs/repository.xml
+  echo "-----> altering configuration file ${repository}"
+  sed -i "s/\*\*port\*\*/${DB_PORT}/g" ${repository}
+  sed -i "s/\*\*host\*\*/${DB_HOST}/g" ${repository}
+  sed -i "s/\*\*jcr_db_name\*\*/${JCR_DB_NAME}/g" ${repository}
+  sed -i "s/\*\*jcr_password\*\*/${JCR_PASS}/g" ${repository}
+  cp -fv ${repository} \
     $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/jackrabbit/repository.xml
 
-  sed -i "s/\*\*port\*\*/${DB_PORT}/g" $PENTAHO_HOME/configs/context.xml 
-  sed -i "s/\*\*host\*\*/${DB_HOST}/g" $PENTAHO_HOME/configs/context.xml   
-  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" $PENTAHO_HOME/configs/context.xml   
-  sed -i "s/\*\*quartz_password\*\*/${QUARTZ_PASS}/g" $PENTAHO_HOME/configs/context.xml 
-  cp -fv $PENTAHO_HOME/configs/context.xml \
+  context=$PENTAHO_HOME/configs/context.xml
+  echo "-----> altering configuration file ${context}"
+  sed -i "s/\*\*port\*\*/${DB_PORT}/g" ${context}
+  sed -i "s/\*\*host\*\*/${DB_HOST}/g" ${context}
+  sed -i "s/\*\*hib_db_name\*\*/${HIBERNATE_DB_NAME}/g" ${context}
+  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" ${context}
+  sed -i "s/\*\*quartz_db_name\*\*/${QUARTZ_DB_NAME}/g" ${context}
+  sed -i "s/\*\*quartz_password\*\*/${QUARTZ_PASS}/g" ${context}
+  cp -fv ${context} \
     $PENTAHO_HOME/pentaho-server/tomcat/webapps/pentaho/META-INF/context.xml
 
-  sed -i "s/\*\*port\*\*/${DB_PORT}/g" $PENTAHO_HOME/configs/applicationContext-spring-security-hibernate.properties  
-  sed -i "s/\*\*host\*\*/${DB_HOST}/g" $PENTAHO_HOME/configs/applicationContext-spring-security-hibernate.properties    
-  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" $PENTAHO_HOME/configs/applicationContext-spring-security-hibernate.properties  
-  cp -fv $PENTAHO_HOME/configs/applicationContext-spring-security-hibernate.properties \
+  hibprop=$PENTAHO_HOME/configs/applicationContext-spring-security-hibernate.properties
+  echo "-----> altering configuration file ${hibprop}"
+  sed -i "s/\*\*port\*\*/${DB_PORT}/g" ${hibprop}
+  sed -i "s/\*\*host\*\*/${DB_HOST}/g" ${hibprop}
+  sed -i "s/\*\*hib_db_name\*\*/${HIBERNATE_DB_NAME}/g" ${hibprop}
+  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" ${hibprop}
+  cp -fv ${hibprop} \
     $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/applicationContext-spring-security-hibernate.properties
 
-  sed -i "s/\*\*port\*\*/${DB_PORT}/g" $PENTAHO_HOME/configs/jdbc.properties  
-  sed -i "s/\*\*host\*\*/${DB_HOST}/g" $PENTAHO_HOME/configs/jdbc.properties    
-  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" $PENTAHO_HOME/configs/jdbc.properties    
-  sed -i "s/\*\*quartz_password\*\*/${QUARTZ_PASS}/g" $PENTAHO_HOME/configs/jdbc.properties  
-  cp -fv $PENTAHO_HOME/configs/jdbc.properties \
+  jdbc=$PENTAHO_HOME/configs/jdbc.properties
+  echo "-----> altering configuration file ${jdbc}"
+  sed -i "s/\*\*port\*\*/${DB_PORT}/g" ${jdbc}
+  sed -i "s/\*\*host\*\*/${DB_HOST}/g" ${jdbc}
+  sed -i "s/\*\*hib_db_name\*\*/${HIBERNATE_DB_NAME}/g" ${jdbc}
+  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" ${jdbc}
+  sed -i "s/\*\*quartz_db_name\*\*/${QUARTZ_DB_NAME}/g" ${jdbc}
+  sed -i "s/\*\*quartz_password\*\*/${QUARTZ_PASS}/g" ${jdbc}
+  cp -fv ${jdbc} \
     $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/simple-jndi/jdbc.properties
 
   sed -i 's/hsql/postgresql/g' \
     $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/hibernate/hibernate-settings.xml
 
-  sed -i "s/localhost/${DB_HOST}/g" $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml 
-  sed -i "s/5432/${DB_PORT}/g" $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml  
-  sed -i "s/>password</>${HIBERNATE_PASS}</g" $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml
+  hibpgcfg=$PENTAHO_HOME/configs/postgresql.hibernate.cfg.xml
+  echo "-----> altering configuration file ${hibpgcfg}"
+  sed -i "s/\*\*port\*\*/${DB_PORT}/g" ${hibpgcfg}
+  sed -i "s/\*\*host\*\*/${DB_HOST}/g" ${hibpgcfg}
+  sed -i "s/\*\*hib_db_name\*\*/${HIBERNATE_DB_NAME}/g" ${hibpgcfg}
+  sed -i "s/\*\*hib_password\*\*/${HIBERNATE_PASS}/g" ${hibpgcfg}
+  cp -fv ${hibpgcfg} \
+    $PENTAHO_HOME/pentaho-server/pentaho-solutions/system/hibernate/postgresql.hibernate.cfg.xml
 
   export PGPASSWORD=$DB_ADMIN_PASS
-  if ! psql -lqt -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT | grep -w hibernate; then
-    echo "-----> creating databases"
 
-	sed -i "s/connect quartz pentaho_user/connect quartz/g" $PENTAHO_HOME/pentaho-server/data/postgresql/create_quartz_postgresql.sql
+  if ! psql -lqt -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT | grep -w $JCR_DB_NAME; then
+    echo "-----> creating database ${JCR_DB_NAME}"
+
+    scriptj=$PENTAHO_HOME/pentaho-server/data/postgresql/create_jcr_postgresql.sql
+    echo "-----> altering script ${scriptj}"
+
+	  sed -i "s/jackrabbit/${JCR_DB_NAME}/g" ${scriptj}
+	  sed -i "s/'password'/'${JCR_PASS}'/g" ${scriptj}
 	
-    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f $PENTAHO_HOME/pentaho-server/data/postgresql/create_jcr_postgresql.sql
-    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f $PENTAHO_HOME/pentaho-server/data/postgresql/create_quartz_postgresql.sql
-    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f $PENTAHO_HOME/pentaho-server/data/postgresql/create_repository_postgresql.sql
+    echo "-----> executing script ${scriptj}"
+    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f ${scriptj}
+  fi
+
+  if ! psql -lqt -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT | grep -w $HIBERNATE_DB_NAME; then
+    echo "-----> creating database ${HIBERNATE_DB_NAME}"
+
+    scripth=$PENTAHO_HOME/pentaho-server/data/postgresql/create_repository_postgresql.sql
+    echo "-----> altering script ${scripth}"
+
+	  sed -i "s/hibernate/${HIBERNATE_DB_NAME}/g" ${scripth}
+	  sed -i "s/'password'/'${HIBERNATE_PASS}'/g" ${scripth}
+
+    echo "-----> executing script ${scripth}"
+    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f ${scripth}
+  fi
+
+  if ! psql -lqt -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT | grep -w $QUARTZ_DB_NAME; then
+    echo "-----> creating database ${QUARTZ_DB_NAME}"
+
+    scriptq=$PENTAHO_HOME/pentaho-server/data/postgresql/create_quartz_postgresql.sql
+    echo "-----> altering script ${scriptq}"
+
+	  sed -i "s/quartz/${QUARTZ_DB_NAME}/g" ${scriptq}
+	  sed -i "s/'password'/'${QUARTZ_PASS}'/g" ${scriptq}
+	  sed -i "s/connect ${QUARTZ_DB_NAME} pentaho_user/connect ${QUARTZ_DB_NAME}/g" ${scriptq}
+	
+    echo "-----> executing script ${scriptq}"
+    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f ${scriptq}
   
     # http://jira.pentaho.com/browse/BISERVER-10639
     # https://github.com/wmarinho/docker-pentaho/blob/5.3/config/postgresql/biserver-ce/data/postgresql/create_quartz_postgresql.sql#L37
-    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT quartz -c 'CREATE TABLE "QRTZ" ( NAME VARCHAR(200) NOT NULL, PRIMARY KEY (NAME) );'
+    psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT $QUARTZ_DB_NAME -c 'CREATE TABLE "QRTZ" ( NAME VARCHAR(200) NOT NULL, PRIMARY KEY (NAME) );'
   fi
-  
-  psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -c "ALTER USER jcr_user WITH PASSWORD '${JCR_PASS}'"
-  psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -c "ALTER USER pentaho_user WITH PASSWORD '${QUARTZ_PASS}'"
-  psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -c "ALTER USER hibuser WITH PASSWORD '${HIBERNATE_PASS}'"
   
   unset PGPASSWORD
 }
--- pentaho-server/8.1/README.md
+++ pentaho-server/8.1/README.md
@@ -1,8 +1,8 @@
 # Description
-Based on [openjdk:8u171](https://hub.docker.com/_/openjdk).
+Based on [openjdk:8-slim](https://hub.docker.com/_/openjdk).
 
-Flexberry/pentaho-8.1 image contains [Pentaho Server 8.1](https://sourceforge.net/projects/pentaho/files/Pentaho%208.1/server/), working with HSQLDB with some modifications:
-- [Microsoft TrueType fonts] installed(https://packages.debian.org/ru/sid/ttf-mscorefonts-installer)
+flexberry/pentaho-8.1 image contains [Pentaho Server 8.1](https://sourceforge.net/projects/pentaho/files/Pentaho%208.1/server/), working with HSQLDB with some modifications:
+- [Microsoft TrueType fonts](https://packages.debian.org/ru/sid/ttf-mscorefonts-installer) installed
 - Sample data creation files removed
 - Updates checking task removed
 - Sample users dropdown removed from Pentaho Server login screen 
@@ -10,3 +10,18 @@ Flexberry/pentaho-8.1 image contains [Pentaho Server 8.1](https://sourceforge.ne
   - Change default passwords for sample users on start (commented corresponding config files copying in Dockerfile). Should implement encryption of provided passwords on docker build by the same algorithm as sample passwords were encrypted.
 
 Pentaho Server is exposed on 8080 port.
+
+# Example
+```
+docker run \
+    --name pentaho \
+    -p 8080:8080 \
+    -d flexberry/pentaho:8.1
+```
+
+```
+docker service create \
+    --name pentaho \
+    -p 8080:8080 \
+    flexberry/pentaho:8.1
+```
