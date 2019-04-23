134bd86d905054a8cd07b20e09111fbe6c7bf433
kafnevod <kaf@ics.perm.ru>
2019-04-23 15:06:43 +0500

    Add background waiting if BOOTUP_CHECK_URL is not null

--- alt.p8-apache2/startApache.sh
+++ alt.p8-apache2/startApache.sh
@@ -11,13 +11,18 @@ do
   a2enmod $module
 done
 
-strace -ff /usr/sbin/httpd2 -D NO_DETACH -k start 2>/tmp/apache.log
-
 if [ -n  "$BOOTUP_CHECK_URL" ]
 then
+  echo "Booting up apache service $BOOTUP_CHECK_URL"
   until wget -c $BOOTUP_CHECK_URL >/dev/null  2>&1
   do
     echo "Wait for start up apache service"
-    sleep 1;
+    sleep 0.1;
   done
-fi
+  echo "Booted up apache service $BOOTUP_CHECK_URL"
+else
+  echo "Skipped booting up"
+fi &
+
+/usr/sbin/httpd2 -D NO_DETACH -k start 2>&1
+
