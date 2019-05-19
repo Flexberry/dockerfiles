7e46e6bbbf2256e6ec0931511ba01c5ca2201d2e
Alexey Kostarev <kaf@nevod.ru>
2019-05-19 16:10:51 +0500

    Debug

--- clickhouse/official/startClickhouse.sh
+++ clickhouse/official/startClickhouse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-
-gosu clickhouse:clickhouse /usr/bin/clickhouse-server --config-file=/etc/clickhouse-server/config.xml}
+#bash
+gosu clickhouse:clickhouse /usr/bin/clickhouse-server --config-file=/etc/clickhouse-server/config.xml
