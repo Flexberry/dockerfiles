cf1b978fdf2c41dbb5e608dee640b4ddb0ae0458
Matvienko Oleg <OMatvienko@ics.perm.ru>
2019-08-07 13:17:20 +0500

    Подключение к БД postgres при проверке и создании БД
    При проверке существования БД и создании БД подключение осуществлялось
    под учеткой $DB_ADMIN_USER без указания имени БД.
    В результате если на сервере отсутствовала БД с именем,
    совпадающим с $DB_ADMIN_USER - подключение не устанавливалось.
    Заменил на явное указание БД postgres в рассчете, что она есть всегда.

--- pentaho/altlinux/configs/postgresql/doSQL.sh
+++ pentaho/altlinux/configs/postgresql/doSQL.sh
@@ -5,5 +5,5 @@ echo "doSQL: File $1:";
 cat $1 >&1
 export PGPASSWORD=$DB_ADMIN_PASS
 echo -ne "Wait connection to database  $DB_ADMIN $DB_HOST:$DB_PORT..."
-until psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -f $1; do sleep 1; done
+until psql -U $DB_ADMIN_USER -h $DB_HOST -p $DB_PORT -d postgres -f $1; do sleep 1; done
 echo
