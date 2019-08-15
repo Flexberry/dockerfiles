61131334fdd916e5e5083699e255e12e535f13e9
Bratchikov Igor <bratchikov@ics.perm.ru>
2019-08-15 10:46:06 +0500

    Remove synchronizationContext creating

--- NewPlatform.Flexberry.ORM.ODataService/Batch/DataObjectODataBatchHandler.cs
+++ NewPlatform.Flexberry.ORM.ODataService/Batch/DataObjectODataBatchHandler.cs
@@ -7,7 +7,6 @@
     using System.Reflection;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Web;
     using System.Web.Http;
     using System.Web.Http.Batch;
     using System.Web.OData.Batch;
@@ -30,9 +29,10 @@
         /// </summary>
         public const string DataObjectsToUpdatePropertyKey = "DataObjectsToUpdate";
 
-        private bool isMono5Runtime = true;
-
-        SynchronizationContext synchronizationContext;
+        /// <summary>
+        /// Flag, indicates that runtime is mono 5.*.
+        /// </summary>
+        private static bool? isMono5Runtime;
 
         /// <summary>
         /// Initializes a new instance of the NewPlatform.Flexberry.ORM.ODataService.Batch.DataObjectODataBatchHandler class.
@@ -42,26 +42,19 @@
         public DataObjectODataBatchHandler(IDataService dataService, HttpServer httpServer)
             : base(httpServer)
         {
-            //if (isMonoRuntime && SynchronizationContext.Current == null)
-            //{
-            //    SynchronizationContext synchronizationContext = new SynchronizationContext();
-            //    SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-            //}
-
-            string str = "constructor-> " + "|" + SynchronizationContext.Current + "|" + TaskScheduler.Current + "|" + Thread.CurrentThread.ManagedThreadId + "|" + HttpContext.Current;
-            LogService.LogError(str);
-
             this.dataService = dataService;
 
             // Mono 5 has problems with async-await calls and correct save HttpContext.Current instance throught tasks threads. This hack need to disable multithreading in batch requests for mono 5.*.
-            isMono5Runtime = IsMono5Runtime();
-
-            if (isMono5Runtime)
+            if (isMono5Runtime == null)
             {
-                synchronizationContext = new SynchronizationContext();
+                isMono5Runtime = IsMono5Runtime();
             }
         }
 
+        /// <summary>
+        /// Is mono 5.* Runtime.
+        /// </summary>
+        /// <returns></returns>
         private bool IsMono5Runtime()
         {
             Type monoRuntimeType = Type.GetType("Mono.Runtime");
@@ -76,7 +69,6 @@
 
                     if (monoVersion.StartsWith("5."))
                     {
-                        LogService.LogError(monoVersion); // TODO: remove it.
                         return true;
                     }
                 }
@@ -85,24 +77,9 @@
             return false;
         }
 
+        /// <inheritdoc />
         public override async Task<HttpResponseMessage> ProcessBatchAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            string newsc = "";
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                synchronizationContext = new SynchronizationContext();
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
-            }
-
-            string str = "ProcessBatchAsync->" + SynchronizationContext.Current + newsc + "|" + TaskScheduler.Current + "|" + Thread.CurrentThread.ManagedThreadId + "|" + HttpContext.Current;
-            LogService.LogError(str);
-
-            if (HttpContext.Current == null)
-            {
-                throw new Exception("Empty HttpContext in DataObjectODataBatchHandler ProcessBatchAsync");
-            }
-
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
@@ -112,7 +89,7 @@
 
             IList<ODataBatchRequestItem> subRequests;
 
-            if (isMono5Runtime)
+            if (isMono5Runtime == true)
             {
                 subRequests = ParseBatchRequestsAsync(request, cancellationToken).Result;
             }
@@ -121,15 +98,9 @@
                 subRequests = await ParseBatchRequestsAsync(request, cancellationToken);
             }
 
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
-            }
-
             try
             {
-                if (isMono5Runtime)
+                if (isMono5Runtime == true)
                 {
                     IList<ODataBatchResponseItem> responses = ExecuteRequestMessagesAsync(subRequests, cancellationToken).Result;
                     return CreateResponseMessageAsync(responses, request, cancellationToken).Result;
@@ -150,18 +121,9 @@
             }
         }
 
+        /// <inheritdoc />
         public override async Task<IList<ODataBatchRequestItem>> ParseBatchRequestsAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            string newsc = "";
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
-            }
-
-            string str = "ParseBatchRequestsAsync->" + SynchronizationContext.Current + newsc + "|" + TaskScheduler.Current + "|" + Thread.CurrentThread.ManagedThreadId + "|" + HttpContext.Current;
-            LogService.LogError(str);
-
             if (request == null)
             {
                 throw new ArgumentNullException(nameof(request));
@@ -176,19 +138,13 @@
 
             ODataMessageReader reader;
 
-            if (isMono5Runtime)
+            if (isMono5Runtime == true)
             {
                 reader = request.Content.GetODataMessageReaderAsync(oDataReaderSettings, cancellationToken).Result;
             }
             else
             {
                 reader = await request.Content.GetODataMessageReaderAsync(oDataReaderSettings, cancellationToken);
-            }
-
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
             }
 
             request.RegisterForDispose(reader);
@@ -202,16 +158,9 @@
                 {
                     IList<HttpRequestMessage> changeSetRequests;
 
-                    if (isMono5Runtime)
+                    if (isMono5Runtime == true)
                     {
                         changeSetRequests = batchReader.ReadChangeSetRequestAsync(batchId, cancellationToken).Result;
-
-                        if (isMono5Runtime && SynchronizationContext.Current == null)
-                        {
-                            SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                            newsc = "*";
-                        }
-
                     }
                     else
                     {
@@ -228,16 +177,9 @@
                 {
                     HttpRequestMessage operationRequest;
 
-                    if (isMono5Runtime)
+                    if (isMono5Runtime == true)
                     {
                         operationRequest = batchReader.ReadOperationRequestAsync(batchId, bufferContentStream: true, cancellationToken: cancellationToken).Result;
-
-                        if (isMono5Runtime && SynchronizationContext.Current == null)
-                        {
-                            SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                            newsc = "*";
-                        }
-
                     }
                     else
                     {
@@ -257,21 +199,6 @@
                    IEnumerable<ODataBatchRequestItem> requests,
                    CancellationToken cancellation)
         {
-            string newsc = "";
-
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
-            }
-
-            string str = "ExecuteRequestMessagesAsync->" + SynchronizationContext.Current + newsc + "|" + TaskScheduler.Current + "|" + Thread.CurrentThread.ManagedThreadId + "|" + HttpContext.Current;
-            LogService.LogError(str);
-            //if (HttpContext.Current == null)
-            //{
-            //    throw new Exception("Empty HttpContext in DataObjectODataBatchHandler ExecuteRequestMessagesAsync");
-            //}
-
             if (requests == null)
             {
                 throw new ArgumentNullException(nameof(requests));
@@ -286,14 +213,9 @@
                     if (operation != null)
                     {
                         ODataBatchResponseItem response;
-                        if (isMono5Runtime)
+                        if (isMono5Runtime == true)
                         {
                             response = request.SendRequestAsync(Invoker, cancellation).Result;
-                            if (isMono5Runtime && SynchronizationContext.Current == null)
-                            {
-                                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                                newsc = "*";
-                            }
                         }
                         else
                         {
@@ -304,15 +226,9 @@
                     }
                     else
                     {
-                        if (isMono5Runtime)
+                        if (isMono5Runtime == true)
                         {
                             ExecuteChangeSet((ChangeSetRequestItem)request, responses, cancellation);
-                            if (isMono5Runtime && SynchronizationContext.Current == null)
-                            {
-                                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                                newsc = "*";
-                            }
-
                         }
                         else
                         {
@@ -346,17 +262,6 @@
         /// <returns>Task for changeset processing.</returns>
         private async Task ExecuteChangeSet(ChangeSetRequestItem changeSet, IList<ODataBatchResponseItem> responses, CancellationToken cancellation)
         {
-            string newsc = "";
-
-            if (isMono5Runtime && SynchronizationContext.Current == null)
-            {
-                SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                newsc = "*";
-            }
-
-            string str = "ExecuteChangeSet->" + SynchronizationContext.Current + newsc + "|" + TaskScheduler.Current + "|" + Thread.CurrentThread.ManagedThreadId + "|" + HttpContext.Current;
-            LogService.LogError(str);
-
             if (changeSet == null)
             {
                 throw new ArgumentNullException(nameof(changeSet));
@@ -373,14 +278,9 @@
             }
 
             ChangeSetResponseItem changeSetResponse;
-            if (isMono5Runtime)
+            if (isMono5Runtime == true)
             {
                 changeSetResponse = (ChangeSetResponseItem)changeSet.SendRequestAsync(Invoker, cancellation).Result;
-                if (isMono5Runtime && SynchronizationContext.Current == null)
-                {
-                    SynchronizationContext.SetSynchronizationContext(synchronizationContext);
-                    newsc = "*";
-                }
             }
             else
             {
