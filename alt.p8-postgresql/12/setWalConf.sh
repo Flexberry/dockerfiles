26a2f983f83f6f3cf887889f2f240efb7dd767a3
Alexey Kostarev <kaf@nevod.ru>
2020-03-05 03:48:34 +0500

    Add setWalConf.sh

---
+++ alt.p8-postgresql/12/setWalConf.sh
@@ -0,0 +1,29 @@
+. ./root/etc/wal-g.d/server.conf
+. ./root/etc/wal-g.d/server-s3.conf
+
+# echo "$WALG_VARS";
+
+ifs=$IFS
+json='{'
+n=0
+for name in $WALG_VARS
+do
+  if [ $n -gt 0 ]
+  then
+    sep=','
+  else
+    sep=''
+  fi
+  eval value=\${$name}
+  re='^[0-9]+$'
+  if  [[ $value =~ $re ]] ; then
+    #echo "$value is number"
+    json="$json$sep\"$name\":$value"
+  else
+    json="$json$sep\"$name\":\"$value\""    
+  fi
+#   echo $var=$value 
+  let n=$n+1
+done
+json="$json}"
+echo $json
