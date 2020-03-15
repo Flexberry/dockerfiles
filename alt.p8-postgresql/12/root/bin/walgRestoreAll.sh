86065838f99f80a5a5afbaa07ab95096f023129c
Alexey Kostarev <kaf@nevod.ru>
2020-03-16 04:30:07 +0500

    Возврат к default значению recovery_target_action

--- alt.p8-postgresql/12/root/bin/walgRestoreAll.sh
+++ alt.p8-postgresql/12/root/bin/walgRestoreAll.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-exec  2>/tmp/walgRestoreAll.log
+# exec  2>/tmp/walgRestoreAll.log
 set >&2
 
 . /etc/wal-g.d/server.conf
@@ -12,17 +12,17 @@ then
   su -c backup-fetch.sh -s /bin/sh postgres
   su -c 'touch /var/lib/pgsql/data/recovery.signal' -s /bin/sh postgres
 
-  POSTGRES_PARAMS="-c restore_command='/bin/wal-fetch.sh %f %p' -c recovery_target_timeline=latest recovery_target_action=shutdown recovery_target_time=$BACKUP_WALG" 
-  su -c "/usr/bin/postgres -D /var/lib/pgsql/data $POSTGRES_PARAMS"  -s /bin/sh postgres #&
+  POSTGRES_PARAMS="-c restore_command='/bin/wal-fetch.sh %f %p' -c recovery_target_timeline=latest recovery_target_time=$BACKUP_WALG" 
+  su -c "/usr/bin/postgres -D /var/lib/pgsql/data $POSTGRES_PARAMS"  -s /bin/sh postgres &
 
-  #sleep 5; \
-  #while [ -f /var/lib/pgsql/data/recovery.signal ]; do sleep 5; done ; \
+  sleep 5; \
+  while [ -f /var/lib/pgsql/data/recovery.signal ]; do sleep 5; done ; \
 
-  #/etc/init.d/postgresql stop; \
-  #sleep 5; \
-  #while [ -f /var/lib/pgsql/data/postmaster.pid ]; do sleep 5; done; \
-  #killall postgres; \
-  #while [ -f /tmp/.s.PGSQL.5432.lock ]; do sleep 5; done;
+  /etc/init.d/postgresql stop; \
+  sleep 5; \
+  while [ -f /var/lib/pgsql/data/postmaster.pid ]; do sleep 5; done; \
+  killall postgres; \
+  while [ -f /tmp/.s.PGSQL.5432.lock ]; do sleep 5; done;
 else
   echo "Режим бекапа BACKUP_WALG: В архиве нет данных для восстановления";
 fi
