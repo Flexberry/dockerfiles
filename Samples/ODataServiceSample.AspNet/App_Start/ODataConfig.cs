fd8f065760e6463f9b6f982358a0510589f59839
leoleopon <safari2002@list.ru>
2020-04-13 10:36:52 +0500

    Add sample AspNet project.

---
+++ Samples/ODataServiceSample.AspNet/App_Start/ODataConfig.cs
@@ -0,0 +1,175 @@
+﻿namespace ODataServiceSample.AspNet
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web;
+    using System.Web.Http;
+    using ICSSoft.Services;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+    using IIS.Caseberry.Logging.Objects;
+    using NewPlatform.Flexberry.AspNet.WebApi.Cors;
+    using NewPlatform.Flexberry.ORM.ODataService.Extensions;
+    using NewPlatform.Flexberry.ORM.ODataService.Functions;
+    using NewPlatform.Flexberry.ORM.ODataService.Model;
+    using NewPlatform.Flexberry.ORM.ODataService.WebApi.Extensions;
+    using NewPlatform.Flexberry.Services;
+    using Unity;
+    using Unity.AspNet.WebApi;
+
+    /// <summary>
+    /// Configure OData Service.
+    /// </summary>
+    internal static class ODataConfig
+    {
+        /// <summary>
+        /// Configure OData by DataObjects assembly.
+        /// </summary>
+        /// <param name="config">Http configuration object.</param>
+        /// <param name="container">Unity container.</param>
+        public static void Configure(HttpConfiguration config, IUnityContainer container, HttpServer httpServer)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException("config");
+            }
+
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+
+            // To support CORS uncomment the line below.
+            config.EnableCors(new DynamicCorsPolicyProvider(true));
+
+            // Use constructor with true first parameter for enable SupportsCredentials.
+
+            // Use Unity as WebAPI dependency resolver
+            config.DependencyResolver = new UnityDependencyResolver(container);
+
+            // Config file upload.
+            config.MapODataServiceFileRoute("File", "api/File", HttpContext.Current.Server.MapPath("~/Uploads"), container.Resolve<IDataService>());
+
+            // Create EDM model builder
+            var assemblies = new[]
+            {
+                Assembly.Load("NewPlatform.Flexberry.ORM.ODataService.Tests.Objects"),
+                typeof(ApplicationLog).Assembly,
+                typeof(UserSetting).Assembly,
+                typeof(Lock).Assembly,
+            };
+            var builder = new DefaultDataObjectEdmModelBuilder(assemblies);
+
+            // Map OData Service
+            var token = config.MapDataObjectRoute(builder, httpServer);
+
+            // User functions
+            token.Functions.Register(new Func<QueryParameters, string>(Test));
+            token.Functions.Register(new Func<string, bool>(ClearLogRecords));
+            token.Functions.RegisterAction(new Func<QueryParameters, string, string, object>(DeleteAllSelect));
+
+            // Event handlers
+            token.Events.CallbackAfterCreate = CallbackAfterCreate;
+        }
+
+        private static void CallbackAfterCreate(DataObject dataObject)
+        {
+            // TODO: implement handler
+        }
+
+        private static string Test(QueryParameters queryParameters)
+        {
+            return "Hello world!";
+        }
+
+        /// <summary>
+        /// OData function for clearing log records.
+        /// </summary>
+        /// <param name="dateTime">Stringed <see cref="DateTimeOffset"/> date. Delete all records older that date.</param>
+        /// <returns>Success operation return <c>true</c>.</returns>
+        private static bool ClearLogRecords(string dateTime)
+        {
+            DateTimeOffset date;
+            if (!DateTimeOffset.TryParse(dateTime, out date))
+            {
+                throw new ArgumentException("Invalid date format", nameof(dateTime));
+            }
+
+            SQLDataService ds = (SQLDataService)DataServiceProvider.DataService;
+            IDbConnection connection = ds.GetConnection();
+            connection.Open();
+            try
+            {
+                //IDbCommand command = connection.CreateCommand();
+                //string tableName = Information.GetClassStorageName(typeof(ApplicationLog));
+                //string timestampColumnName = Information.GetPropertyStorageName(
+                //    typeof(ApplicationLog),
+                //    Information.ExtractPropertyName<ApplicationLog>(a => a.Timestamp));
+                //command.CommandText = $"DELETE FROM {tableName} WHERE {timestampColumnName} <= @timestamp";
+                //SqlParameter timestampParameter = new SqlParameter("@timestamp", SqlDbType.DateTimeOffset) { Value = date };
+                //command.Parameters.Add(timestampParameter);
+                //command.ExecuteScalar();
+                return true;
+            }
+            finally
+            {
+                // Close the connection if that's how we got it.
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// OData function for delete all select records.
+        /// </summary>
+        /// <param name="queryParameters">Request OData Parameters.</param>
+        /// <param name="pathName">Type name.</param>
+        /// <param name="filterQuery">Query for filter.</param>
+        /// <returns>Number of deleted records.</returns>
+        private static object DeleteAllSelect(QueryParameters queryParameters, string pathName, string filterQuery)
+        {
+            try
+            {
+                SQLDataService dataService = DataServiceProvider.DataService as SQLDataService;
+
+                var uri = $"http://a/b/c?{filterQuery}";
+                Type type = queryParameters.GetDataObjectType(pathName);
+                LoadingCustomizationStruct lcs = queryParameters.CreateLcs(type, uri);
+                DataObject[] updateObjects = dataService.LoadObjects(lcs);
+                int deletedCount = updateObjects.Length;
+
+                for (var i = 0; i < updateObjects.Length; i++)
+                {
+                    updateObjects[i].SetStatus(ObjectStatus.Deleted);
+                }
+
+                DataObject[] updateObjectsArray = updateObjects.ToArray();
+                dataService.UpdateObjects(ref updateObjectsArray);
+
+                return new
+                {
+                    deletedCount,
+                    message = string.Empty
+                };
+            }
+            catch (Exception e)
+            {
+                Exception ex = e;
+                var msg = ex.Message;
+                while (ex.InnerException != null)
+                {
+                    ex = ex.InnerException;
+                    msg += Environment.NewLine + ex.Message;
+                }
+
+                return new
+                {
+                    deletedCount = -1,
+                    message = msg
+                };
+            }
+        }
+    }
+}
---
+++ Samples/ODataServiceSample.AspNet/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿namespace ODataServiceSample.AspNet
+{
+    using System;
+    using System.Web;
+    using System.Web.Http;
+
+    using ICSSoft.STORMNET.Business;
+
+    using Microsoft.Practices.Unity.Configuration;
+    using Unity;
+
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            IUnityContainer container = new UnityContainer();
+            container.LoadConfiguration();
+            container.RegisterInstance(DataServiceProvider.DataService);
+            GlobalConfiguration.Configure(configuration => ODataConfig.Configure(configuration, container, GlobalConfiguration.DefaultServer));
+        }
+    }
+}
